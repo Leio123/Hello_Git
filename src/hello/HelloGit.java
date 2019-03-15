86907ce627b2540c6fc7caa37d0196358ec923d2
yanglei <1035786243@qq.com>
2019-03-15 12:56:05 +0800

    First commit all.

---
+++ src/hello/HelloGit.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * @ClassName HelloGit
+ * @Description TODO
+ * @Author Leio
+ * @Date 2019/3/15 11:58
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git, I come from IEDA.");
+    }
+}
